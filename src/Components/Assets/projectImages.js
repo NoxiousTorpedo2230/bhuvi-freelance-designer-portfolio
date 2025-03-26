dc5f145cde3a5c0e87ae741959d88c89c6f2fd84
Moses-Stephen-1922 <moses.s2203@gmail.com>
2025-03-26 21:11:43 +0530

    projectImages.js

--- src/Components/Assets/projectImages.js
+++ src/Components/Assets/projectImages.js
@@ -1,77 +1,41 @@
-import Design1 from "./20231216_201343_0001.png";
-import Design2 from "./20231216_201406_0000.png";
-import Design3 from "./20240117_112226_0001.png";
-import Design4 from "./20240122_185005_0000.jpg";
-import Design5 from "./20240122_185005_0001.jpg";
-import Design6 from "./20240122_185005_0002.jpg";
-import Design7 from "./20240122_185005_0003.jpg";
-import Design8 from "./20240316_134611_0000.jpg";
-import Design9 from "./Summer time_20240430_191908_0000.jpg";
-import Design10 from "./New dress collection_20240420_110045_0000.jpg";
-import Design11 from "./Cream Elegant New Arrival Instagram Post_20240402_091104_0000.jpg";
-import Design12 from "./Add a heading_20240506_170036_0002.jpg";
 
-const projectImages = [
-  {
-    image: Design1,
-    title: "Brand Identity Design",
-    category: "Logo & Branding",
-  },
-  {
-    image: Design2,
-    title: "Packaging Design",
-    category: "Product Design",
-  },
-  {
-    image: Design3,
-    title: "Social Media Poster",
-    category: "Digital Marketing",
-  },
-  {
-    image: Design4,
-    title: "Creative Logo Design",
-    category: "Logo & Branding",
-  },
-  {
-    image: Design5,
-    title: "Brochure Design",
-    category: "Print Design",
-  },
-  {
-    image: Design6,
-    title: "E-commerce Thumbnails",
-    category: "Digital Design",
-  },
-  {
-    image: Design7,
-    title: "Product Packaging Design",
-    category: "Product Design",
-  },
-  {
-    image: Design8,
-    title: "Seasonal Promotion Graphics",
-    category: "Social Media Design",
-  },
-  {
-    image: Design9,
-    title: "Summer Collection Promotion",
-    category: "Social Media Design",
-  },
-  {
-    image: Design10,
-    title: "New Dress Collection Poster",
-    category: "Fashion Design",
-  },
-  {
-    image: Design11,
-    title: "Elegant New Arrival Poster",
-    category: "Social Media Design",
-  },
-  {
-    image: Design12,
-    title: "Promotional Banner",
-    category: "Advertising Design",
-  },
-];
+//Package Design
+import packageDesign1 from "./20231216_201343_0001.png";
+import packageDesign2 from "./20231216_201406_0000.png";
 
+//Brand Design
+import brandDesign1 from "./20240117_112226_0001.png";
+import brandDesign2 from "./20240122_185005_0000.jpg";
+import brandDesign3 from "./20240122_185005_0001.jpg";
+import brandDesign4 from "./20240122_185005_0002.jpg";
+import brandDesign5 from "./20240122_185005_0003.jpg";
+
+//Instagram Posters
+import instaPostersDesign1 from "./20240316_134611_0000.jpg";
+import instaPostersDesign2 from "./Summer time_20240430_191908_0000.jpg";
+import instaPostersDesign3 from "./New dress collection_20240420_110045_0000.jpg";
+import instaPostersDesign4 from "./Cream Elegant New Arrival Instagram Post_20240402_091104_0000.jpg";
+import instaPostersDesign5 from "./Add a heading_20240506_170036_0002.jpg";
+
+
+const projectImages=[
+  {
+    title: "Package Design",
+    images:[
+      packageDesign1, packageDesign2
+    ]
+  },
+  {
+    title: "Brand Design",
+    images: [
+      brandDesign1, brandDesign2, brandDesign3, brandDesign4, brandDesign5
+    ]
+  },
+  {
+    title: "Instagram Posters",
+    images: [
+      instaPostersDesign1, instaPostersDesign2, instaPostersDesign3, instaPostersDesign4, instaPostersDesign5
+    ]
+  }
+]
 export default projectImages;
