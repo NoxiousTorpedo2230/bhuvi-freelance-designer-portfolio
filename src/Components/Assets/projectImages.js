e2e18caf0595ea4badaff3b22d09711b2090dc62
Moses-Stephen-1922 <moses.s2203@gmail.com>
2025-03-30 07:21:49 +0530

    projectImages.js

--- src/Components/Assets/projectImages.js
+++ src/Components/Assets/projectImages.js
@@ -2,6 +2,7 @@
 //Package Design
 import packageDesign1 from "./20231216_201343_0001.png";
 import packageDesign2 from "./20231216_201406_0000.png";
+import packageDesign3 from "./Copy of Untitled Design_20250327_111948_0004.jpg";
 
 //Brand Design
 import brandDesign1 from "./20240117_112226_0001.png";
@@ -17,12 +18,19 @@ import instaPostersDesign3 from "./New dress collection_20240420_110045_0000.jpg
 import instaPostersDesign4 from "./Cream Elegant New Arrival Instagram Post_20240402_091104_0000.jpg";
 import instaPostersDesign5 from "./Add a heading_20240506_170036_0002.jpg";
 
+//Client Work Brochure
+import clientWorkBrochure1 from "./Brown Modern New Collection Brochure_20250328_104927_0006.jpg";
+import clientWorkBrochure2 from "./3_20250328_130551_0008.png";
+import clientWorkBrochure3 from "./Brown Modern New Collection Brochure_20250328_104927_0005.jpg";
+import clientWorkBrochure4 from "./2_20250328_130551_0007.png";
+import clientWorkBrochure5 from "./20250328_132054_0002.png";
+
 
 const projectImages=[
   {
     title: "Package Design",
     images:[
-      packageDesign1, packageDesign2
+      packageDesign1, packageDesign2, packageDesign3
     ]
   },
   {
@@ -35,6 +43,12 @@ const projectImages=[
     title: "Instagram Posters",
     images: [
       instaPostersDesign1, instaPostersDesign2, instaPostersDesign3, instaPostersDesign4, instaPostersDesign5
+    ]
+  },
+  {
+    title: "Client Work Brochure",
+    images : [
+      clientWorkBrochure1, clientWorkBrochure2, clientWorkBrochure3, clientWorkBrochure4, clientWorkBrochure5
     ]
   }
 ]
